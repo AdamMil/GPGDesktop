1f58048be9b2530d2ccc6e0930690a3c0afb3535
adam <adam@6bc1db4b-3406-4559-80b5-07b83529ab72>
2011-06-14 11:37:43 +0000

    DecryptVerifyWizard.cs: * Used new Stream.CopyTo() extension
    KeyFormWithOptions.cs:
    KeyFormWithOptions.Designer.cs:
    KeyFormWithOptions.resx:
    * Added a new key selection form with an additional option
    
    MainForm.cs:
    * Allowed explicit encryption to self when encrypting in the scratch pad
    
    OptionsForm.cs:
    * Look in "Program Files (x86)" as well as "Program Files"

---
+++ KeyFormWithOptions.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GPGDesktop
+{
+  partial class KeyFormWithOptions
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if(disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.chkEncryptToSelf = new System.Windows.Forms.CheckBox();
+      this.SuspendLayout();
+      // 
+      // chkEncryptToSelf
+      // 
+      this.chkEncryptToSelf.AutoSize = true;
+      this.chkEncryptToSelf.Checked = true;
+      this.chkEncryptToSelf.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.chkEncryptToSelf.Location = new System.Drawing.Point(11, 63);
+      this.chkEncryptToSelf.Name = "chkEncryptToSelf";
+      this.chkEncryptToSelf.Size = new System.Drawing.Size(312, 17);
+      this.chkEncryptToSelf.TabIndex = 5;
+      this.chkEncryptToSelf.Text = "Encrypt to &myself (i.e. use this key as a recipient)";
+      this.chkEncryptToSelf.UseVisualStyleBackColor = true;
+      // 
+      // KeyFormWithOptions
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+      this.ClientSize = new System.Drawing.Size(519, 92);
+      this.Controls.Add(this.chkEncryptToSelf);
+      this.Name = "KeyFormWithOptions";
+      this.Controls.SetChildIndex(this.chkEncryptToSelf, 0);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.CheckBox chkEncryptToSelf;
+  }
+}
---
+++ KeyFormWithOptions.cs
@@ -0,0 +1,24 @@
+﻿using AdamMil.Security.UI;
+using AdamMil.Security.PGP;
+
+namespace GPGDesktop
+{
+  public partial class KeyFormWithOptions : KeyForm
+  {
+    public KeyFormWithOptions()
+    {
+      InitializeComponent();
+    }
+
+    public KeyFormWithOptions(string description, params PrimaryKey[] keys) : base(description, keys)
+    {
+      InitializeComponent();
+    }
+
+    public bool EncryptToSelf
+    {
+      get { return chkEncryptToSelf.Checked; }
+      set { chkEncryptToSelf.Checked = value; }
+    }
+  }
+}
